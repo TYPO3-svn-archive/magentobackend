5eae80ff6eecf6e20e672f05af427a4a8afc9962
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-30 21:28:40 +0000

    new filestructure only, extension magento !not working yet!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/magentobackend/trunk@10843 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ mod/magentosoap/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/magentoconnect/mod1/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['name']='web_txmagentoconnectM1';
+
+	
+$MCONF['access']='admin';
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:magentoconnect/mod1/locallang_mod.xml';
+?>
